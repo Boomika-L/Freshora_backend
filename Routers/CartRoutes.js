2b6ddd62c05a9372831b6deb64f0e595f239b2a4
Boomika.L <boomika.l2024cse@sece.ac.in>
2026-06-07 19:51:42 +0530

    Add route to get cart items by email

--- Routers/CartRoutes.js
+++ Routers/CartRoutes.js
@@ -4,13 +4,14 @@ const {
   addToCart,
   getCartItems,
   deleteCartItem,
+  getCart,
   updateQuantity
 } = require("../Controllers/CartController");
 
 const router = express.Router();
 
 router.post("/add", addToCart);
-
+router.get("/all/:email", getCart); 
 router.get("/all", getCartItems);
 
 router.delete("/delete/:id", deleteCartItem);
