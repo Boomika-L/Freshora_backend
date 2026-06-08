7e15159eaec3d2a552ad2e80e752b7eb6114da5b
Boomika.L <boomika.l2024cse@sece.ac.in>
2026-06-08 12:58:53 +0530

    Refactor server.js for improved structure and routes

--- server.js
+++ server.js
@@ -1,64 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
-app.use(cors());
-app.listen(5000,()=>{
-  console.log("server running in Port 5000");
-});
-const userroutes = require("./Routers/UserRoutes")
-const productroutes = require("./Routers/ProductRoutes");
-const ProductRoutes = require(
-  "./Routers/ProductRoutes"
-);
-const cartRoutes = require("./Routers/CartRoutes");
-app.use("/api/user",userroutes);
-app.use("/api/products",productroutes);
-app.use(
-  "/api/product",
-  ProductRoutes
-);
-const profileRoutes =
-require("./Routers/ProfileRoutes");
+app.use("/api/user", require("./Routers/UserRoutes"));
+app.use("/api/products", require("./Routers/ProductRoutes"));
+app.use("/api/product", require("./Routers/ProductRoutes"));
+app.use("/api/profile", require("./Routers/ProfileRoutes"));
+app.use("/api/admin", require("./Routers/AdminRoutes"));
+app.use("/api/dashboard", require("./Routers/DashboardRoutes"));
+app.use("/api/wishlist", require("./Routers/WishlistRoutes"));
+app.use("/api/orders", require("./Routers/OrderRoutes"));
+app.use("/api/offers", require("./Routers/OfferRoutes"));
+app.use("/api/cart", require("./Routers/CartRoutes"));
 
-app.use(
-"/api/profile",
-profileRoutes
-);
-const adminRoutes =
-require("./Routers/AdminRoutes");
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
 
-app.use(
-"/api/admin",
-adminRoutes
-);
-const dashboardRoutes =
-require("./Routers/DashboardRoutes");
-
-app.use(
-  "/api/dashboard",
-  dashboardRoutes
-);
-const wishlistRoutes =
-require("./Routers/WishlistRoutes");
-
-app.use(
-  "/api/wishlist",
-  wishlistRoutes
-);
-const orderRoutes = require("./Routers/OrderRoutes");
-const offerRoutes = require("./Routers/OfferRoutes");
-
-app.use("/api/offers", offerRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/cart", cartRoutes);
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("mongodb connected successfully");
-}
-    ).catch((err)=>{
-        console.log("mongodb connection failed");
-    })
-
-
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server Running");
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed");
+    console.log(err);
+  });
